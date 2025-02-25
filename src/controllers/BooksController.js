636a4ac344011dacc0cf0e2ed7ab8a9772c2dc62
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-25 19:10:25 -0300

    feat: add BooksController

---
+++ src/controllers/BooksController.js
@@ -0,0 +1,15 @@
+
+let books = [
+    {
+        id: 1,
+        title: "The Fellowship of The Ring",
+        author: "J.R.R. Tolkien",
+    }
+]
+
+export const BooksController = {
+    listBooks(req, res){
+        res.writeHead(200, {"content-type": "application.json"})
+        res.end(JSON.stringify(books))
+    }
+}
